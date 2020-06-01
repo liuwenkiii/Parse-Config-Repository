66df99213336233f77d6bd88bdf6fa8c46347882
liuwenkiii <liuwenkiii@gmail.com>
2020-06-01 21:53:17 +0800

    添加四川工程技术大学测试

---
+++ schools/四川工程技术学院/afterLogin.js
@@ -0,0 +1,6 @@
+loadUrlWithHeader(
+    'http://jiaowu.scetc.edu.cn/xuesheng/gongxuan/gongxuan/kbbanji.asp',
+    {
+        referer: 'http://jiaowu.scetc.edu.cn/xuesheng/gongxuan/gongxuan/kai.asp?xueqi=2019-2020-2'
+    }
+);
