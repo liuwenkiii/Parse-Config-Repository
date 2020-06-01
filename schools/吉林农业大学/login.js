0d28c7272ce146d9b0cd4530ffbda04f7f30cb47
liuwenkiii <liuwenkiii@gmail.com>
2020-06-01 16:26:20 +0800

    fix

--- schools/吉林农业大学/login.js
+++
@@ -1,8 +0,0 @@
-let http = new XMLHttpRequest();
-http.open('POST', host + '/admin/login');
-http.send('username=' + account + '&password=' + password);
-http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-        loadUrl(afterLoginUrl);
-    }
-}
