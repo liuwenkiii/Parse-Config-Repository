bd6fc0d21042cd827bfaeab5b7937f092abd828f
liuwenkiii <liuwenkiii@gmail.com>
2020-06-02 03:37:12 +0800

    修复黑龙江职业学院

--- schools/黑龙江职业学院/login.js
+++ schools/黑龙江职业学院/login.js
@@ -1,2 +1,8 @@
-document.getElementsByTagName('input')[0].value = account;
-document.getElementsByTagName('input')[1].value = password;
+let http = new XMLHttpRequest();
+http.open('GET', 'http://222.171.247.92:9801/datacenter/user/login.do?username=' + account + '&password=' + password + '&verificationCode=&isRememberLoginInfo=false&webUrl=%2Flogin.html');
+http.send();
+http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+        loadUrl('http://222.171.247.92:9801/portal/index.html');
+    }
+}
