d12a592dccb2283b5bb05c774b344c9fe692f592
liuwenkiii <liuwenkiii@gmail.com>
2020-06-01 16:04:47 +0800

    修复吉林农业大学登录方式

--- schools/吉林农业大学/login.js
+++ schools/吉林农业大学/login.js
@@ -1,5 +1,5 @@
 let http = new XMLHttpRequest();
-http.open('POST', '/admin/login');
+http.open('POST', host + '/admin/login');
 http.send('username=' + account + '&password=' + password);
 http.onreadystatechange = function() {
     if (http.readyState == 4 && http.status == 200) {
